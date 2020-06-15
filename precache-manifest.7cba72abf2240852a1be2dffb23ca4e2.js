67a34d0516b041e33129975d08e4b2d56cb269cd
Tomas <tomasftrotti@gmail.com>
2020-06-15 10:36:40 -0300

    Updates

---
+++ precache-manifest.7cba72abf2240852a1be2dffb23ca4e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5edc2d4aad0780dc5be0fc2be227560",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "f5381134388634a39e97",
+    "url": "/monsters-rolodex-react/static/css/main.aabb7968.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/monsters-rolodex-react/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-react/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5381134388634a39e97",
+    "url": "/monsters-rolodex-react/static/js/main.0faf5821.chunk.js"
+  },
+  {
+    "revision": "a561a666e1086893a8f3",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.2d49fe8c.js"
+  }
+]);
